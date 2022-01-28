1ad3925f3cbfb59c8e085972357319d35a02fc68
Benno Evers <benno@lampi>
2022-01-28 22:41:20 +0000

    Add brightness control

--- src/bin/shell-controller.rs
+++ src/bin/shell-controller.rs
@@ -1,14 +1,15 @@
 fn main() {
     let strands = vec![39, 31, 38, 20];
     let mut device = firelight::Handle::new(5, 0, 18, strands);
-    let mut state = false;
     println!("press enter to toggle light status");
     let mut input = String::new();
+    let mut brightness: u8 = 128;
     loop {
         std::io::stdin()
             .read_line(&mut input)
             .expect("error: unable to read user input");
-        state = !state;
-        device.control(firelight::Control { on: state });
+        device.toggle();
+        brightness += 20;
+        device.adjust_brightness(brightness);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,19 @@ use std::sync::mpsc;
 
 pub struct Control {
     pub on: bool,
+    brightness: u8,
     // TODO:
-    //brightness: u8;
     //color_rgb: (u8, u8, u8);
 }
 
 pub struct Handle {
     thread: Option<std::thread::JoinHandle<()>>,
     tx: mpsc::Sender<ControlMsg>,
+
+    // Remember the last-sent state so we can offer
+    // convenience methods to toggle on/off, adjust
+    // brightness, etc.
+    state: Control,
 }
 
 enum ControlMsg {
@@ -30,34 +35,12 @@ struct ControlThreadData {
     state: Control,
 }
 
-fn light_control_thread(mut data: ControlThreadData) -> () {
-    loop {
-        let mut idx = 0;
-        let colors_on = vec![0xffffff, 0xffffff, 0xffffff, 0xffffff];
-        let colors_off = vec![0x0, 0x0, 0x0, 0x0];
-        let colors = if data.state.on { colors_on } else { colors_off };
-            //println!("applying light state {} with colors {:?}", data.state.on, colors);
-        // TODO: support multiple channels
-        for (i, led) in data.hw.channel_mut(0).leds_mut().iter_mut().enumerate() {
-            while idx < data.strands_cum.len() && i >= data.strands_cum[idx] {
-                idx += 1;
-            }
-            if idx >= colors.len() {
-                break;
-            }
-            *led = colors[idx];
-        }
-
-        data.hw.render().unwrap();
-        data.hw.wait().unwrap();
-
-        let msg = data.rx.recv().unwrap();
-        match msg {
-            ControlMsg::Shutdown => break,
-            ControlMsg::External(control) => data.state = control,
-        }
-    }
+impl Control {
+  fn default() -> Control {
+    return Control {on: false, brightness: 255};
+  }
 }
+
 
 impl Handle {
     pub fn new(
@@ -90,13 +73,12 @@ impl Handle {
                 .build()
                 .unwrap();
 
-            let default_state = Control { on: false };
             let control_data = ControlThreadData {
                 rx: rx,
                 hw: handler,
                 strands: strands,
                 strands_cum: cumsum,
-                state: default_state,
+                state: Control::default(),
             };
             return light_control_thread(control_data);
         });
@@ -104,17 +86,30 @@ impl Handle {
         return Handle {
             thread: Some(join_handle),
             tx: tx,
+            state: Control::default(),
         };
     }
 
+    // fully set state
     pub fn control(&mut self, control: Control) {
         let _ = self.tx.send(ControlMsg::External(control));
     }
 
+    // convenience functions
+    pub fn toggle(&mut self) {
+        let toggled = Control{on: !self.state.on, brightness: self.state.brightness};
+        self.control(toggled);
+    }
+
+    pub fn adjust_brightness(&mut self, brightness: u8) {
+        let adjusted = Control{on: self.state.on, brightness: brightness};    
+        self.control(adjusted);
+    }
+
     pub fn drop(&mut self) {
+        let _ = self.tx.send(ControlMsg::Shutdown);
         // This is apparently a standard idiom known as the "option dance" [1]
         // [1]: https://users.rust-lang.org/t/spawn-threads-and-join-in-destructor/1613/9
-        let _ = self.tx.send(ControlMsg::Shutdown);
         if let Some(handle) = self.thread.take() {
             match handle.join() {
                 Ok(_) => (),
@@ -123,3 +118,33 @@ impl Handle {
         }
     }
 } // impl handle
+
+fn light_control_thread(mut data: ControlThreadData) -> () {
+    loop {
+        let mut idx = 0;
+
+        let colors_on = vec![0xffffff, 0xffffff, 0xffffff, 0xffffff];
+        let colors_off = vec![0x0, 0x0, 0x0, 0x0];
+        let colors = if data.state.on { colors_on } else { colors_off };
+        data.hw.channel_mut(0).set_brightness(data.state.brightness);
+        // TODO: support multiple channels
+        for (i, led) in data.hw.channel_mut(0).leds_mut().iter_mut().enumerate() {
+            while idx < data.strands_cum.len() && i >= data.strands_cum[idx] {
+                idx += 1;
+            }
+            if idx >= colors.len() {
+                break;
+            }
+            *led = colors[idx];
+        }
+
+        data.hw.render().unwrap();
+        data.hw.wait().unwrap();
+
+        let msg = data.rx.recv().unwrap();
+        match msg {
+            ControlMsg::Shutdown => break,
+            ControlMsg::External(control) => data.state = control,
+        }
+    }
+}
